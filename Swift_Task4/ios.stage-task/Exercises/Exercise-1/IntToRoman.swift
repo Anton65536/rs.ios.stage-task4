a252d3a27abdc1ba3f83e5d696d593c1c7ca1bc9
Anton Petrash <antonpetrash@Antons-MacBook-Air.local>
2021-06-04 07:24:43 +0300

    init: add intToRoman solution

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,28 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
-    }
+        if self < 1 {
+            return nil
+        }
+
+        let decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+            let numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+            var result = ""
+            var number = self
+
+            while number > 0
+            {
+                for (index, decimal) in decimals.enumerated()
+                {
+                    if number - decimal >= 0 {
+                        number -= decimal
+                        result += numerals[index]
+                        break
+                    }
+                }
+            }
+
+        return result
+}
 }
